26862ce49cee633dad2b5805c102432ef22504de
aakanksha-arun <desai.aaka@northeastern.edu>
2024-02-14 23:02:27 -0500

    tf files and workflow

---
+++ main.tf
@@ -0,0 +1,29 @@
+#Creating VPC
+
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project_name
+  name                    = "vpc-${var.env}"
+  auto_create_subnetworks = false
+  routing_mode = "REGIONAL"
+  delete_default_routes_on_create = true
+}
+resource "google_compute_subnetwork" "subnet-webapp" {
+  name          = "webapp"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_subnetwork" "subnet-db" {
+  name          = "db"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_route" "webapp-route" {
+  name = "webapp-route"
+  network = google_compute_network.vpc_network.id
+  dest_range = "0.0.0.0/0"
+  next_hop_gateway = "default-internet-gateway"
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "project_name"{
+type = string
+}
+
+variable "region"{
+type = string
+default = "us-east1"
+}
+
+variable "env"{
+type = string
+}
+
